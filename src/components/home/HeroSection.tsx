6c8857ca33ad4598db65757c8af1a159a42373dd
vietlvhe180475 <vietlvhe180475@fpt.edu.vn>
2025-05-28 12:14:45 +0700

    Task STT 2

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 import React from 'react';
 import { useState } from 'react';
 import ModalVideo from 'react-modal-video';
-import thumb1 from "../../../public/assets/img/banner/banner-1.jpg"
-import thumb2 from "../../../public/assets/img/banner/banner-line.png"
+import thumb1 from "../../../public/assets/img/banner/banner-1-image.png"
+import thumb2 from "../../../public/assets/img/banner/banner-line-image.png"
 import Image from 'next/image';
 import HeroSlider from './HeroSlider';
 const HeroSection = () => {
@@ -22,22 +22,22 @@ const HeroSection = () => {
                     <div className="col-xxl-7 col-xl-6 col-lg-6">
                         <div className="bd-banner__content-box mb-60">
                             <div className="bd-banner__text">
-                                <h2>Pure Organic <br />Food Shop</h2>
+                                <h2>VƯỜN RAU <br />SẠCH TẬN TAY</h2>
                             </div>
                             <div className="row">
                                 <div className="col-xxl-5">
                                 </div>
                                 <div className="col-xxl-6">
-                                    <ModalVideo channel='youtube' isOpen={isOpen} videoId='vWLcyFtni6U' onClose={() => { openVideoModal(); }} />
+                                    <ModalVideo channel='youtube' isOpen={isOpen} videoId='gVtEwbJt1HE' onClose={() => { openVideoModal(); }} />
                                     <div className="bd-banner__content__wrap">
                                         <div className="bd-banner__content">
-                                            <p>Organic foods include fresh produce, meats, and dairy products as well as processed foods such as fruits frozen meals.</p>
+                                            <p>Chúng tôi không chỉ cung cấp cây xanh tươi tốt cho không gian sống mà còn hướng đến lối sống lành mạnh, gần gũi với thiên nhiên.</p>
                                             <div className="bd-banner__button">
                                                 <div className="bd-button__inner">
-                                                    <Link className="bd-bn__btn-1" href="/shop">Shop Now</Link>
+                                                    <Link className="bd-bn__btn-1" href="/shop">Mua hàng</Link>
                                                 </div>
                                                 <div className="bd-banner__link">
-                                                    <span className="banner-video__btn popup-video" onClick={() => { openVideoModal(); }}><i className="fa-solid fa-play"></i><span>Watch our<br />videos</span></span>
+                                                    <span className="banner-video__btn popup-video" onClick={() => { openVideoModal(); }}><i className="fa-solid fa-play"></i><span>Xem video<br /></span></span>
                                                 </div>
                                             </div>
                                         </div>
